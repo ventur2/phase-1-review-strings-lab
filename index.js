4052c3724295376bae1a633eb06f1116860637bf
rachell <dazzlemevanity@gmail.com>
2022-05-18 15:09:45 -0400

    commit tree 6ec9ef0fe22f9240228832cba9c507ccff3c8ddf

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper!'
+const excitedWelcomeMessage = 'WECLOME TO FLATBOOK, Grace Hopper!'
